68cb79ef33df5941d18ddaf468874512f1adc786
Punith1117 <punith11172006@gmail.com>
2025-08-15 17:25:11 +0530

    feat(setup): install and setup express, cors and dotenv

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require('cors')
+const app = express();
+
+require('dotenv').config();
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
+app.get("/health", (req, res) => res.status(200).json({status: 'ok'}));
+
+app.listen(process.env.PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Express app listening on port ${process.env.PORT}!`);
+});
